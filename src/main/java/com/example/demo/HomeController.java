be84a6252a59846e8e68f24104462daf8e0d0362
GBTC441019UR <GBTC441019UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-03-15 09:26:54 -0400

    lesson complete

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,39 @@
+package com.example.demo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.jws.WebParam;
+import java.util.HashSet;
+import java.util.Set;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    ActorRepository actorRepository;
+
+    @Autowired
+    MovieRepository movieRepository;
+
+    @RequestMapping("/")
+    public String loadData(Model model){
+
+        Actor actor = new Actor();
+        actor.setName("Sandra Bullock");
+        actor.setRealname("Sandra Mae Bullowski");
+        //actorRepository.save(actor);
+
+        Movie movie = new Movie();
+        movie.setTitle("Emoji Movie");
+        movie.setYear(2017);
+        movie.setDescription("About Emojis...");
+
+        actor.addMovie(movie);
+        actorRepository.save(actor);
+
+        model.addAttribute("actors", actorRepository.findAll());
+        return "index";
+    }
+}
